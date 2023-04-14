aea8a77acb17802d6c760cb8d233a73245cd0526
kienns-work <gacongay289@gmail.com>
2023-04-14 21:51:56 +0700

    finished Home_screen

---
+++ lib/core/data/destinations_data.dart
@@ -0,0 +1,16 @@
+import '../helpers/asset_helper.dart';
+
+class Destinations {
+  static final List<Map<String, String>> listImageLeft = [
+    {'name': 'Hà Nội', 'image': AssetHelper.imageIntro1},
+    {'name': 'Đà Nẵng', 'image': AssetHelper.imageIntro1},
+    {'name': 'Đà Nẵng', 'image': AssetHelper.imageIntro2},
+    {'name': 'Đà Nẵng', 'image': AssetHelper.imageIntro3},
+  ];
+  static final List<Map<String, String>> listImageRight = [
+    {'name': 'Hội An', 'image': AssetHelper.imageIntro3},
+    {'name': 'Hải Dương', 'image': AssetHelper.imageIntro2},
+    {'name': 'Hải Dương', 'image': AssetHelper.imageBackgroundSplash},
+    {'name': 'Hải Dương', 'image': AssetHelper.imageIntro3}
+  ];
+}
--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:bookinghotel/core/constants/colors_constants.dart';
 import 'package:bookinghotel/core/constants/dimension_constants.dart';
+import 'package:bookinghotel/core/constants/textstyle_constants.dart';
 import 'package:bookinghotel/core/helpers/asset_helper.dart';
 import 'package:bookinghotel/core/helpers/image_helper.dart';
 import 'package:bookinghotel/representation/widgets/app_bar_container.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../core/data/destinations_data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -40,6 +43,70 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ]),
+      ),
+    );
+  }
+
+  Widget _buildImage(String name, String image) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: EdgeInsets.only(bottom: kMediumPadding),
+        child: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            ImageHelper.loadFromAsset(
+              image,
+              width: double.infinity,
+              fit: BoxFit.fitWidth,
+              radius: BorderRadius.circular(kMediumPadding),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(kDefaultPadding / 2),
+              child: Icon(
+                Icons.favorite,
+                color: Colors.white,
+              ),
+            ),
+            Positioned(
+              left: kDefaultPadding,
+              bottom: kDefaultPadding,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyles.defaultStyle.whiteTextColor.bold,
+                  ),
+                  SizedBox(
+                    height: kItemPadding,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(kMinPadding),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(kMinPadding),
+                      color: Colors.white.withOpacity(0.4),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(
+                          Icons.star,
+                          color: Color(0xffFFC107),
+                          size: 12,
+                        ),
+                        SizedBox(
+                          width: kItemPadding,
+                        ),
+                        Text('4.5')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -164,6 +231,50 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
             ],
+          ),
+          SizedBox(height: kMediumPadding),
+          Row(
+            children: [
+              Text(
+                'Popular Destinations',
+                style: TextStyles.defaultStyle.bold,
+              ),
+              Spacer(),
+              Text(
+                'See All',
+                style: TextStyle(
+                  color: ColorPalette.primaryColor,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: kMediumPadding,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Expanded(
+                  child: Column(
+                    children: Destinations.listImageLeft
+                        .map((e) => _buildImage(e['name']!, e['image']!))
+                        .toList(),
+                  ),
+                ),
+                SizedBox(
+                  width: kMediumPadding,
+                ),
+                Expanded(
+                  child: Column(
+                    children: Destinations.listImageRight
+                        .map((e) => _buildImage(e['name']!, e['image']!))
+                        .toList(),
+                  ),
+                ),
+              ]),
+            ),
           )
         ],
       ),
